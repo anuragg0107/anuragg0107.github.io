aa3ebe9c9547dea0bff1266f479b37b293bc1923
anurag gupta <101393446+anuragg0107@users.noreply.github.com>
2022-12-25 18:38:48 +0530

    push

--- src/component/Gitgraph.jsx
+++ src/component/Gitgraph.jsx
@@ -6,16 +6,16 @@ function Gitgraph(){
 
     return (
         <>
-            <Box fontWeight={"bold"}  >
+            <Box fontWeight={"bold"} className='github_bold'  >
                 <Center p={"12"} >
                     {/* <Heading fontSize={'48px'} fontFamily='Robot'>My GitHub Calender</Heading> */}
                     <Text className='github_data' fontSize={'2.8rem'} fontFamily='Robot'  >My <strong className='strong'>GitHub</strong> Calender</Text>
                 </Center>
                 <Githubcalendar
                     username="anuragg0107"
-                    blockSize={18}
-                    blockMargin={5}
-                    fontSize={16}
+                    // blockSize={1}
+                    // blockMargin={5}
+                    // fontSize={16}
                     color={"green"}
                     width='90%'
                     className='data'
